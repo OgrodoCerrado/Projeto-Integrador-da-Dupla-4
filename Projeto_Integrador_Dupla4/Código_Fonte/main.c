b365c0166072046ed23dc09b9b624f98dce33b57
OgrodoCerrado <Igorpechir@yahoo.com.br>
2025-11-16 16:34:40 -0300

    Update main.c

--- Projeto_Integrador_Dupla4/Código_Fonte/main.c
+++ Projeto_Integrador_Dupla4/Código_Fonte/main.c
@@ -6,128 +6,316 @@
 // INICIO - Declaração de variáveis e apresentação do menu inicial
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
-	char regiaoEntrada[15], regiaoPratoPrincipal[15], regiaoSobremesa[15];
-	char tipoConsumidor[12];
-	char entradaEscolhida[15], pratoPrincipalEscolhido[15], sobremesaEscolhida[15];
+	int opcaoMenu = 0;
+	int regiaoEntrada = 0, regiaoPratoPrincipal = 0, regiaoSobremesa = 0;
+	char tipoConsumidor[50] = "geral";
+	int entradaEscolhidaNumerica = 0, pratoPrincipalEscolhidoNumerico = 0, sobremesaEscolhidaNumerica = 0;
+	char entradaEscolhida[50] = "Nao selecionada", pratoPrincipalEscolhido[50] = "Nao selecionado", sobremesaEscolhida[50] = "Nao selecionada";
 
-	printf("\n=== LISTA DE RECEITAS NACIONAIS ===\n");
-	printf("\n| ENTRADA | PRATO PRINCIPAL | SOBREMESA |\n");
-	printf("\n| NORTE | NORDESTE | CENTRO-OESTE | SUDESTE | SUL |\n");
+	printf("\n=========== LISTA DE RECEITAS NACIONAIS ==========\n");
+	printf("\nSeja bem vindo ao sistema de lista de receitas da culinaria brasileira!\n");
+	printf("\nQue tipo de consumidor voce se identifica? \n");
 	printf("\n| GERAL | VEGETARIANO | VEGANO |\n");
-	printf("Ao responder, escreva em letra minuscula todas as respostas.\n");
-	printf("Caso não queira escolher um tipo de prato, apenas aperte ENTER \n");
+	scanf("%49s", tipoConsumidor);
 
-// PARTE 2 - Seleção de regiões e tipo de consumidor 
+	do 
+	{
+		printf("Escolha uma das opcoes a seguir: \n");
+		printf("\n| (1) ENTRADA | (2) PRATO PRINCIPAL | (3) SOBREMESA | (4) SAIR\n");
+		scanf("%d", &opcaoMenu);
+
+		switch (opcaoMenu) 
+		{
+			case 1:
+				printf("\n================ ENTRADAS =================\n");
+				printf("\n| (1) NORTE | (2) NORDESTE | (3) CENTRO-OESTE | (4) SUDESTE | (5) SUL |\n");
+				printf("Em que região brasileira você deseja procurar a sua entrada? \n");
+				scanf("%d", &regiaoEntrada);
+
+				switch (regiaoEntrada) 
+					{
+						case 1:
+							printf("1 - Tacaca \n2 - Vatapa\n");
+							printf("Qual entrada voce deseja selecionar? \n");
+							scanf("%d", &entradaEscolhidaNumerica);
+							switch (entradaEscolhidaNumerica)
+							{
+							case 1:
+								strcpy(entradaEscolhida[50], "Tacaca");
+								break;
+							case 2:
+								strcpy(entradaEscolhida[50], "Vatapa");
+							default:
+								break;
+							}
+						break;
+						case 2:
+							printf("1 - Cuscuz \n2 - Acaraje\n");
+							printf("Qual entrada voce deseja selecionar? \n");
+							scanf("%d", &entradaEscolhidaNumerica);
+							switch (entradaEscolhidaNumerica)
+							{
+							case 1:
+								strcpy(entradaEscolhida[50], "Cuscuz");
+								break;
+							case 2:
+								strcpy(entradaEscolhida[50], "Acaraje");
+							default:
+								break;
+							}
+						break;
+						case 3:
+							printf("1 - Quiabo \n2 - Chipa\n");
+							printf("Qual entrada voce deseja selecionar? \n");
+							scanf("%d", &entradaEscolhidaNumerica);
+							switch (entradaEscolhidaNumerica)
+							{
+							case 1:
+								strcpy(entradaEscolhida[50], "Quiabo");
+								break;
+							case 2:
+								strcpy(entradaEscolhida[50], "Chipa");
+							default:
+								break;
+							}
+						break;
+						case 4:
+							printf("1 - Pao de queijo \n2 - Coxinha\n");
+							printf("Qual entrada voce deseja selecionar? \n");
+							scanf("%d", &entradaEscolhidaNumerica);
+							switch (entradaEscolhidaNumerica)
+							{
+							case 1:
+								strcpy(entradaEscolhida[50], "Pao de queijo");
+								break;
+							case 2:
+								strcpy(entradaEscolhida[50], "Coxinha");
+							default:
+								break;
+							}
+						break;
+						case 5:
+							printf("1 - Pinhao \n2 - Tainha\n");
+							printf("Qual entrada voce deseja selecionar? \n");
+							scanf("%d", &entradaEscolhidaNumerica);
+							switch (entradaEscolhidaNumerica)
+							{
+							case 1:
+								strcpy(entradaEscolhida[50], "Pinhao");
+								break;
+							case 2:
+								strcpy(entradaEscolhida[50], "Tainha");
+							default:
+								break;
+							}
+						break;
+						default:
+							printf("Regiao invalida ou não selecionada.\n");
+						break;
+					}
+			break;
+			
+			case 2:
+					printf("\n============== PRATOS PRINCIPAIS ===============\n");
+					printf("\n| (1) NORTE | (2) NORDESTE | (3) CENTRO-OESTE | (4) SUDESTE | (5) SUL |\n");
+					printf("Em que região brasileira você deseja procurar o seu prato principal? \n");
+					scanf("%d", &regiaoPratoPrincipal);
+
+				switch (regiaoPratoPrincipal) 
+					{
+						case 1:
+							printf("1 - Manicoba \n2 - Pirarucu\n");
+							printf("Qual prato voce deseja selecionar? \n");
+							scanf("%d", &pratoPrincipalEscolhidoNumerico);
+							switch (pratoPrincipalEscolhidoNumerico)
+							{
+							case 1:
+								strcpy(pratoPrincipalEscolhido[50], "Manicoba");
+								break;
+							case 2:
+								strcpy(pratoPrincipalEscolhido[50], "Pirarucu");
+							default:
+								break;
+							}
+						break;
+						case 2:
+							printf("1 - Moqueca \n2 - Baiao\n");
+							printf("Qual prato voce deseja selecionar? \n");
+							scanf("%d", &pratoPrincipalEscolhidoNumerico);
+							switch (pratoPrincipalEscolhidoNumerico)
+							{
+							case 1:
+								strcpy(pratoPrincipalEscolhido[50], "Moqueca");
+								break;
+							case 2:
+								strcpy(pratoPrincipalEscolhido[50], "Baiao");
+							default:
+								break;
+							}
+						break;
+						case 3:
+							printf("1 - Arroz com pequi \n2 - Galinhada\n");
+							printf("Qual prato voce deseja selecionar? \n");
+							scanf("%d", &pratoPrincipalEscolhidoNumerico);
+							switch (pratoPrincipalEscolhidoNumerico)
+							{
+							case 1:
+								strcpy(pratoPrincipalEscolhido[50], "Arroz com pequi");
+								break;
+							case 2:
+								strcpy(pratoPrincipalEscolhido[50], "Galinhada");
+							default:
+								break;
+							}
+						break;
+						case 4:
+							printf("1 - Feijoada \n2 - Virado Paulista\n");
+							printf("Qual prato voce deseja selecionar? \n");
+							scanf("%d", &pratoPrincipalEscolhidoNumerico);
+							switch (pratoPrincipalEscolhidoNumerico)
+							{
+							case 1:
+								strcpy(pratoPrincipalEscolhido[50], "Feijoada");
+								break;
+							case 2:
+								strcpy(pratoPrincipalEscolhido[50], "Virado Paulista");
+							default:
+								break;
+							}
+						break;
+						case 5:
+							printf("1 - Churrasco \n2 - Galeto\n");
+							printf("Qual prato voce deseja selecionar? \n");
+							scanf("%d", &pratoPrincipalEscolhidoNumerico);
+							switch (pratoPrincipalEscolhidoNumerico)
+							{
+							case 1:
+								strcpy(pratoPrincipalEscolhido[50], "Churrasco");
+								break;
+							case 2:
+								strcpy(pratoPrincipalEscolhido[50], "Galeto");
+							default:
+								break;
+							}
+						break;
+						default:
+							printf("Regiao invalida ou não selecionada.\n");
+						break;
+					}
+			break;
+			
+			case 3:
+					printf("\n================ SOBREMESAS =================\n");
+					printf("\n| (1) NORTE | (2) NORDESTE | (3) CENTRO-OESTE | (4) SUDESTE | (5) SUL |\n");
+					printf("Em que região brasileira você deseja procurar a sua sobremesa? \n");
+					scanf("%d", &regiaoSobremesa);
+					
+					switch (regiaoPratoPrincipal) 
+						{
+							case 1:
+								printf("1 - Cupuacu \n2 - Acai\n");
+								printf("Qual sobremesa voce deseja selecionar? \n");
+								scanf("%d", &sobremesaEscolhidaNumerica);
+								switch (sobremesaEscolhidaNumerica)
+									{
+									case 1:
+										strcpy(sobremesaEscolhida[50], "Cupuacu");
+										break;
+									case 2:
+										strcpy(sobremesaEscolhida[50], "Acai");
+									default:
+										break;
+									}
+							break;
+							case 2:
+								printf("1 - Cocada \n2 - Mugunza\n");
+								printf("Qual sobremesa voce deseja selecionar? \n");
+								scanf("%d", &sobremesaEscolhidaNumerica);
+								switch (sobremesaEscolhidaNumerica)
+									{
+									case 1:
+										strcpy(sobremesaEscolhida[50], "Cocada");
+										break;
+									case 2:
+										strcpy(sobremesaEscolhida[50], "Mugunza");
+									default:
+										break;
+									}
+							break;
+							case 3:
+								printf("1 - Furrundum \n2 - Pamonha\n");
+								printf("Qual sobremesa voce deseja selecionar? \n");
+								scanf("%d", &sobremesaEscolhidaNumerica);
+								switch (sobremesaEscolhidaNumerica)
+									{
+									case 1:
+										strcpy(sobremesaEscolhida[50], "Furrundum");
+										break;
+									case 2:
+										strcpy(sobremesaEscolhida[50], "Pamonha");
+									default:
+										break;
+									}
+							break;
+							case 4:
+								printf("1 - Doce de leite \n2 - Goiabada\n");
+								printf("Qual sobremesa voce deseja selecionar? \n");
+								scanf("%d", &sobremesaEscolhidaNumerica);
+								switch (sobremesaEscolhidaNumerica)
+									{
+									case 1:
+										strcpy(sobremesaEscolhida[50], "Doce de leite");
+										break;
+									case 2:
+										strcpy(sobremesaEscolhida[50], "Goiabada");
+									default:
+										break;
+									}
+							break;
+							case 5:
+								printf("1 - Cuca \n2 - Grostoli\n");
+								printf("Qual sobremesa voce deseja selecionar? \n");
+								scanf("%s", &sobremesaEscolhidaNumerica);
+								switch (sobremesaEscolhidaNumerica)
+									{
+									case 1:
+										strcpy(sobremesaEscolhida[50], "Cuca");
+										break;
+									case 2:
+										strcpy(sobremesaEscolhida[50], "Grostoli");
+									default:
+										break;
+									}
+							break;
+							default:
+								printf("Regiao invalida ou não selecionada.\n");
+							break;
+						}
+			break;
+
+				case 4:
+				printf("\nSaindo do sistema de seleção de receitas...\n");
+				break;
+
+				default:
+				printf("\nOpcao de menu invalida. Por favor, escolha 1, 2, 3 ou 4.\n");
+				break;
+			}
+	} while (opcaoMenu != 4);
 		
-	printf("Em que região brasileira você deseja procurar a sua entrada? \n");
-	scanf("%14s", regiaoEntrada);
-	
-	printf("Em que região brasileira você deseja procurar o seu prato principal? \n");
-	scanf("%14s", regiaoPratoPrincipal);
-
-	printf("Em que região brasileira você deseja procurar a sua sobremesa? \n");
-	scanf("%14s", regiaoSobremesa);
-
-	printf("Que tipo de consumidor voce se identifica? \n");
-	scanf("%11s", tipoConsumidor);
-
-// Após o usuário escolher as regiões e identificar o tipo de consumidor que o usuário decidiu, ele será apresentado com a lista de receitas que deverão ser escolhidas
-// PARTE 3 - Listagem e Seleção de receitas baseadas na escolha que foi feita na parte 2
-	printf("\n================ ENTRADAS =================\n");
-
-	int comparacaoEntradaNorte = strcmp(regiaoEntrada, "norte");
-	int comparacaoEntradaNordeste = strcmp(regiaoEntrada, "nordeste");
-	int comparacaoEntradaCentroOeste = strcmp(regiaoEntrada, "centro-oeste");
-	int comparacaoEntradaSudeste = strcmp(regiaoEntrada, "sudeste");
-	int comparacaoEntradaSul = strcmp(regiaoEntrada, "sul");
-
-	if (comparacaoEntradaNorte == 0) {
-        printf("1 - Tacaca \n2 - Vatapa\n");
-    } else if (comparacaoEntradaNordeste == 0) {
-        printf("1 - Cuscuz \n2 - Acaraje\n");
-    } else if (comparacaoEntradaCentroOeste == 0) {
-        printf("1 - Quiabo \n2 - Chipa\n");
-    } else if (comparacaoEntradaSudeste == 0) {
-        printf("1 - Pao_de_queijo \n2 - Coxinha\n");
-    } else if (comparacaoEntradaSul == 0) {
-        printf("1 - Pinhao \n2 - Tainha\n");
-    } else {
-        printf("Regiao invalida ou não selecionada. Seguindo o código...\n");
-    }
-
-	if (comparacaoEntradaNorte == 0 || comparacaoEntradaNordeste == 0 || comparacaoEntradaCentroOeste == 0 || comparacaoEntradaSudeste == 0 || comparacaoEntradaSul == 0)  {
-		printf("Qual entrada voce deseja selecionar? Digite o nome da entrada. \n");
-		scanf("%s",entradaEscolhida);
-	}
-
-// O usuário irá escolher a partir da listagem apresentada uma entrada e após a seleção do prato principal e da sobremesa, que seguem o mesmo sistema de listagem e seleção
-	printf("\n============== PRATOS PRINCIPAIS ===============\n");
-	int comparacaoPPNorte = strcmp(regiaoPratoPrincipal, "norte");
-	int comparacaoPPNordeste = strcmp(regiaoPratoPrincipal, "nordeste");
-	int comparacaoPPCentroOeste = strcmp(regiaoPratoPrincipal, "centro-oeste");
-	int comparacaoPPSudeste = strcmp(regiaoPratoPrincipal, "sudeste");
-	int comparacaoPPSul = strcmp(regiaoPratoPrincipal, "sul");
-
-	if (comparacaoPPNorte == 0) {
-        printf("1 - Manicoba \n2 - Pirarucu\n");
-    } else if (comparacaoPPNordeste == 0) {
-        printf("1 - Moqueca \n2 - Baiao\n");
-    } else if (comparacaoPPCentroOeste == 0) {
-        printf("1 - Arroz_com_Pequi \n2 - Galinhada\n");
-    } else if (comparacaoPPSudeste == 0) {
-        printf("1 - Feijoada \n2 - Virado_Paulista\n");
-    } else if (comparacaoPPSul == 0) {
-        printf("1 - Churrasco \n2 - Galeto\n");
-    } else {
-        printf("Regiao invalida ou não selecionada. Seguindo o código...\n");
-    }
-
-	if (comparacaoPPNorte == 0 || comparacaoPPNordeste == 0 || comparacaoPPCentroOeste == 0 || comparacaoPPSudeste == 0 || comparacaoPPSul == 0)  {
-		printf("Qual prato principal voce deseja selecionar? Digite o nome dele. \n");
-		scanf("%s", pratoPrincipalEscolhido);
-	}
-
-
-	printf("\n================ SOBREMESAS =================\n");
-	int comparacaoSobremesaNorte = strcmp(regiaoSobremesa, "norte");
-	int comparacaoSobremesaNordeste = strcmp(regiaoSobremesa, "nordeste");
-	int comparacaoSobremesaCentroOeste = strcmp(regiaoSobremesa, "centro-oeste");
-	int comparacaoSobremesaSudeste = strcmp(regiaoSobremesa, "sudeste");
-	int comparacaoSobremesaSul = strcmp(regiaoSobremesa, "sul");
-
-	if (comparacaoSobremesaNorte == 0) {
-        printf("1 - Cupuacu \n2 - Acai\n");
-    } else if (comparacaoSobremesaNordeste == 0) {
-        printf("1 - Cocada \n2 - Mugunza\n");
-    } else if (comparacaoSobremesaCentroOeste == 0) {
-        printf("1 - Furrundum \n2 - Pamonha\n");
-    } else if (comparacaoSobremesaSudeste == 0) {
-        printf("1 - Doce_de_leite \n2 - Goiabada\n");
-    } else if (comparacaoSobremesaSul == 0) {
-        printf("1 - Cuca \n2 - Grostoli\n");
-    } else {
-        printf("Regiao invalida ou não selecionada. Seguindo o código...\n");
-    }
-
-	if (comparacaoSobremesaNorte == 0 || comparacaoSobremesaNordeste == 0 || comparacaoSobremesaCentroOeste == 0 || comparacaoSobremesaSudeste == 0 || comparacaoSobremesaSul == 0)  {
-		printf("Qual sobremesa voce deseja selecionar? Digite o nome dela. \n");
-		scanf("%s",	sobremesaEscolhida);
-	}
-
-
-// Após a seleção exata de cada receita, elas serao apresentadas apos um menu com intuito de separar cada etapa do código
-// Aqui serão exibidas em uma interface final todas opções selecionadas pelo usuário
-
 	printf("\n======================================================\n");
-	printf("O usuário é consumidor %s\n", tipoConsumidor);
-	printf("Entrada escolhida: %s\n", entradaEscolhida);
-	printf("Prato principal escolhido: %s\n", pratoPrincipalEscolhido);
-	printf("Sobremesa escolhida: %s\n", sobremesaEscolhida);
+	printf("RESUMO DO SEU CARDÁPIO\n");
+	printf("======================================================\n");
+	printf("O usuário é consumidor %s\n", tipoConsumidor[50]);
+	printf("Entrada escolhida: %s\n", entradaEscolhida[50]);
+	printf("Prato principal escolhido: %s\n", pratoPrincipalEscolhido[50]);
+	printf("Sobremesa escolhida: %s\n", sobremesaEscolhida[50]);
 	printf("Tenha uma otima refeicao, espero que tenha um otimo tempo cozinhando as receitas!");
-
+	
 // FIM
 		return 0;
 }
